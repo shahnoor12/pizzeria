b24cf65dc3d6a1dcfa63f1ee7394a51536221a99
shahnoor12 <86612506+shahnoor12@users.noreply.github.com>
2022-04-13 22:55:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+menu_list_array = ["Veg Margherita Pizza",
+                    "chicken tandoori pizza",
+                    "paneer pizza",
+                    "veg-maharaja pizza",
+                    "corn pizza",
+                    "mutton pizza"
+                    ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        htmldata=menu_list_array.sort()
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
